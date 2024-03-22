14e7a4a1c6e0ac9553bbfdaa49401fc8da071804
LuoYuMeow <2843966358@qq.com>
2024-03-22 21:38:35 +0800

    First Commit For My Eww Config

---
+++ script/date.sh
@@ -0,0 +1,24 @@
+export LANG=en_US.UTF-8
+case $1 in
+  -H )
+    date "+ %H" | sed 's/ //g'
+    ;;
+  -M )
+    date "+ %M" | sed 's/ //g'
+    ;;
+  -S )
+    date "+ %S" | sed 's/ //g'
+    ;;
+  -Y )
+    date "+ %Y" | sed 's/ //g'
+    ;;
+  -m )
+    date "+ %m" | sed 's/ //g'
+    ;;
+  -d )
+    date "+ %d" | sed 's/ //g'
+    ;;
+  -a )
+    date "+ %a" | sed 's/ //g'
+    ;;
+esac
